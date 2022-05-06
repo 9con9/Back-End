35ae02c6741b3d68af6ba8ee62ea6bc2c8f12104
Hwanghuntae <47139385+Hwanghuntae@users.noreply.github.com>
2022-05-06 14:00:17 +0900

    dangn and naver parsing code

---
+++ dangn.py
@@ -0,0 +1,60 @@
+import pymysql
+import requests
+from bs4 import BeautifulSoup
+import re
+from selenium import webdriver
+import time
+
+
+
+
+
+def get_dangn(keyword):
+    # 이모티콘 제거하기
+    pattern = re.compile("["
+                         u"\U00010000-\U0010FFFF"  # BMP characters 이외
+                         "]+", flags=re.UNICODE)
+
+    url = "https://www.daangn.com/search/" + keyword
+    # url = "https://www.daangn.com/search/천안%20아이패드%20에어3"
+
+    # 변수 초기화
+    name, address, price, link, img_link = [], [], [], [], []
+
+    # 셀레니움
+    driver = webdriver.Chrome('chromedriver.exe')
+    driver.implicitly_wait(time_to_wait=5)
+    driver.get(url)
+    page = driver.page_source
+    soup = BeautifulSoup(page, "html.parser")
+    driver.find_element_by_xpath("//*[@id=\"result\"]/div[1]/div[2]/span").click()
+    driver.find_element_by_xpath("//*[@id=\"result\"]/div[1]/div[2]/span").click()
+    driver.implicitly_wait(time_to_wait=5)
+
+
+    for i in range(2, 31):
+        print(i)
+
+        name.append(driver.find_elements_by_xpath("//*[@id='flea-market-wrap']/article[" + str(i) + "]/a/div[2]/div/span[1]")[0].text)
+        address.append(driver.find_elements_by_xpath("//*[@id='flea-market-wrap']/article[" + str(i) + "]/a/div[2]/p[1]")[0].text.strip())
+        price.append(driver.find_elements_by_xpath("//*[@id='flea-market-wrap']/article[" + str(i) + "]/a/div[2]/p[2]")[0].text.strip())
+        link.append(driver.find_elements_by_xpath("//*[@id='flea-market-wrap']/article[" + str(i) + "]/a")[0].get_attribute('href'))
+        img_link.append(driver.find_elements_by_xpath("//*[@id='flea-market-wrap']/article[" + str(i) + "]/a/div[1]/img")[0].get_attribute("src"))
+
+    # DB 연결하기
+    conn = pymysql.connect(host="127.0.0.1", user="root", password="1234", db="condb", charset="utf8")
+
+    # DB 커서 만들기
+    cursor = conn.cursor(pymysql.cursors.DictCursor)
+
+    # sql 문
+    sql = "INSERT INTO condb.UserSells VALUES(%s, %s, %s, %s, %s, %s, %s)"
+
+    # db에 sql
+    for i in range(len(name)):
+        cursor.execute(sql,
+                       (i + 1, '당근 마켓', pattern.sub(r"", name[i]), address[i], price[i], str(link[i]), img_link[i]))
+
+    conn.commit()
+
+
---
+++ naver.py
@@ -0,0 +1,67 @@
+import pymysql
+import requests
+from bs4 import BeautifulSoup
+import re
+from selenium import webdriver
+import time
+
+def get_naver(keyword):
+    # 이모티콘 제거하기
+    pattern = re.compile("["
+            u"\U00010000-\U0010FFFF"  #BMP characters 이외
+                               "]+", flags=re.UNICODE)
+
+    # 변수 초기화
+    name, upload_time, price, link, img_link, address = [], [], [], [], [], []
+
+    url = "https://m.joongna.com/search-list/product?searchword=" + keyword
+
+    # 셀레니움
+    driver = webdriver.Chrome('chromedriver.exe')
+    driver.implicitly_wait(time_to_wait=5)
+    # 중고나라 접속
+    driver.get(url)
+    page = driver.page_source
+    soup = BeautifulSoup(page, "html.parser")
+    driver.find_element_by_xpath('//*[@id="root"]/div[1]/section/article/button').click()
+    time.sleep(1)
+    driver.find_element_by_xpath('//*[@id="root"]/div[1]/section/article/button').click()
+    time.sleep(1)
+    driver.find_element_by_xpath('//*[@id="root"]/div[1]/section/article/button').click()
+    time.sleep(1)
+    driver.find_element_by_xpath('//*[@id="root"]/div[1]/section/article/button').click()
+    time.sleep(1)
+    driver.find_element_by_xpath('//*[@id="root"]/div[1]/section/article/button').click()
+    time.sleep(1)
+
+    for i in range(2,10):
+        print(i)
+        try:
+            address.append(driver.find_elements_by_xpath('//*[@id="root"]/div[1]/section/article/div/div[' + str(i) + ']/div/div/a/div/p[2]')[0].text)
+        except:
+            address.append("None")
+        name.append(driver.find_elements_by_xpath('//*[@id="root"]/div[1]/section/article/div/div[' + str(i) + ']/div/div/a/span')[0].text)
+        upload_time.append(driver.find_elements_by_xpath('// *[ @ id = "root"] / div[1] / section / article / div / div[' + str(i) + '] / div / div / a / div / p')[0].text)
+        price.append(driver.find_elements_by_xpath('// *[ @ id = "root"] / div[1] / section / article / div / div[' + str(i) + '] / div / div / a / p')[0].text)
+        link.append(driver.find_elements_by_xpath('// *[ @ id = "root"] / div[1] / section / article / div / div[' + str(i) + '] / div / a')[0].get_attribute('href'))
+        try:
+            img_link.append(driver.find_elements_by_xpath('//*[@id="root"]/div[1]/section/article/div/div[' + str(i) + ']/div/a/div/img')[0].get_attribute("src"))
+        except:
+            img_link.append(driver.find_elements_by_xpath('//*[@id="root"]/div[1]/section/article/div/div[' + str(i) + ']/div/div/a')[0].get_attribute("href"))
+
+    # DB 연결하기
+    conn = pymysql.connect(host="127.0.0.1", user="root", password="1234", db="condb", charset="utf8")
+
+    # DB 커서 만들기
+    cursor = conn.cursor(pymysql.cursors.DictCursor)
+
+    # sql 문
+    sql = "INSERT INTO condb.naver_usersells VALUES(%s, %s, %s, %s, %s, %s, %s, %s)"
+
+    # db에 sql
+    for i in range(len(name)):
+       cursor.execute(sql, (i+1, '중고 나라', pattern.sub(r"",name[i]), upload_time[i], str(address[i]), price[i], str(link[i]), img_link[i]))
+
+    conn.commit()
+
+
