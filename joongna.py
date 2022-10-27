53f29fabd29ee1d13120cf81cc386e8c89567c97
Hwanghuntae <47139385+Hwanghuntae@users.noreply.github.com>
2022-10-27 18:40:29 +0900

    add location search

--- joongna.py
+++ joongna.py
@@ -1,25 +1,51 @@
-from ctypes import addressof
-from email.mime import image
 from selenium import webdriver
 from bs4 import BeautifulSoup
 import chromedriver_autoinstaller
 import re
 import numpy as np
+import pandas as pd
+from time import sleep
+from geopy.geocoders import Nominatim
+
+def geocoding(address):
+    geolocoder = Nominatim(user_agent = 'South Korea', timeout=None)
+    geo = geolocoder.geocode(address)
+    crd = {"lat": str(geo.latitude), "lng": str(geo.longitude)}
+
+    return crd
 
 def get_joongna(search_keyword):
     
     result = []
 
+    ###
+    spl = search_keyword.split()
+    list_spl = [k for k in spl]
+    location_temp = list_spl[0:1]
+    joongna_keyword_temp = list_spl[1:]
+    location = " ".join(location_temp)
+    joongna_keyword = " ".join(joongna_keyword_temp)
+    ###
+
+    crd = geocoding(location)
+    lat = crd['lat']
+    lon = crd['lng']
+
     options = webdriver.ChromeOptions()
     options.add_argument('--window-size=1920x1080')
+    options.add_argument('--incognito')
     options.add_argument('--headless')
     options.add_argument('--no-sandbox')
+    options.add_argument('--disable-setuid-sandbox')
     options.add_argument('--disable-dev-shm-usage')
+    options.add_argument("--remote-debugging-port=9222")
     options.add_experimental_option("excludeSwitches", ["enable-logging"])
     
-    driver = webdriver.Chrome(chromedriver_autoinstaller.install(), options=options)
+    path = 'C:/Users/H_T/Desktop/공부/프로젝트/창경진_구구콘/개발/쓰래드/Back-End/chromedriver.exe'
+    driver = webdriver.Chrome(path)#, options=options)
+    driver.get('https://web.joongna.com/search?keyword=' + joongna_keyword + '&lat=' + lat + '&lon=' + lon +'&page=1')
     driver.implicitly_wait(3)
-    driver.get('https://web.joongna.com/search?keyword=' + search_keyword + '&page=1')
+    sleep(3)
 
     html = driver.page_source
     soup = BeautifulSoup(html, 'html.parser')
@@ -45,7 +71,10 @@ def get_joongna(search_keyword):
                     price_div = second.find_all(attrs={'class': 'priceTxt'})
                     for price in price_div:
                         prices = re.sub(r'[^0-9]', '', price.get_text())
-                        price_list.append(prices)
+                        if len(prices) == 0:
+                            price_list.append(0)
+                        else:
+                            price_list.append(int(prices))
                     title_div = second.find_all(attrs={'class': 'titleTxt'})
                     for title in title_div:
                         name_list.append(title.get_text())
@@ -53,7 +82,6 @@ def get_joongna(search_keyword):
                     for time in time_div:
                         spans = time.find_all('span')
                         flag = True
-                        print(time)
                         for span in spans:
                             if len(time) == 1:
                                 tim = span.get_text()
@@ -68,65 +96,28 @@ def get_joongna(search_keyword):
                                     tim = span.get_text()
                                     upload_time_list.append(tim)
                                     
-    print(len(name_list), upload_time_list, len(price_list), len(link_list), len(img_link_list), address_list)
-    # for i in items:
-    #     divs = i.find_all("div")
-    #     for div in divs:
-    #         link_div = div.find_all(attrs={'class': 'sc-chbbiW dlCCUH'})
-    #         for link in link_div:
-    #             isAD = div.find_all(attrs={'class':'sc-jXQZqI fwwrJI'})
-    #             AD = []
-    #             for ad in isAD:
-    #                 if ad.get_text() == 'AD':
-    #                     AD.append(ad)
-    #             if len(AD) == 0:
-    #                 href = link.attrs['href']
-                    
-    #                 link_list.append("https://bunjang.co.kr" + href)
-                    
-    #                 imgs = link.find('img')
-    #                 img = imgs['src']
-
-    #                 img_link_list.append(img)
-
-    #                 price_div = div.find_all(attrs={'class': "sc-gmeYpB iBMbn"})
-    #                 if len(price_div) == 0:
-    #                         price_list.append('0')
-    #                 else:
-    #                     for price in price_div:
-    #                         prices = re.sub(r'[^0-9]', '', price.get_text())
-    #                         price_list.append(prices)
-    #                 name_div = div.find_all(attrs={'class': "sc-fcdeBU iVCsji"})
-    #                 for name in name_div:
-    #                     name_list.append(name.get_text())
-    #                 place_div = div.find_all(attrs={'class': "sc-kZmsYB eylVEY"})
-    #                 for place in place_div:
-    #                     address_list.append(place.get_text())
-    #                 time_div = div.find_all(attrs={'class': "sc-iSDuPN iJqnGY"})
-    #                 for time in time_div:
-    #                     upload_time_list.append(time.get_text())
-    # print(name_list, upload_time_list, price_list, link_list, img_link_list, address_list)
-                        
-    # print(len(name_list))
     temp_list = price_list
     np_temp = np.array(temp_list, dtype=np.int64)
-    Q3, Q1, Q2 = np.percentile(np_temp, [75, 25, 50])
+    pd_temp = pd.Series(np_temp)
+    Q3 = pd_temp.quantile(.75)
+    Q1 = pd_temp.quantile(.25)
+    Q2 = pd_temp.quantile(.5)
     IQR = Q3 - Q1
-    print(Q3, Q1, Q2, IQR)
     if IQR > Q2:
         low_np = list(np_temp[Q1 > np_temp])
         high_np = list(np_temp[Q3 < np_temp])
     else:
-        low_np = list(np_temp[Q1-0.4*IQR > np_temp])
+        low_np = list(np_temp[Q1-0.2*IQR > np_temp])
         high_np = list(np_temp[Q3+0.4*IQR < np_temp])
-    print(Q3+1*IQR, Q1-1*IQR, Q2, IQR)
 
     for i in range(len(name_list)):
-        prices = int(re.sub(r'[^0-9]', '', price_list[i]))
+        prices = price_list[i]
         if prices in low_np:
-            result.append([i+1, '번개 장터', name_list[i], upload_time_list[i], str(address_list[i]), int(prices), str(link_list[i]), img_link_list[i], 'low'])
+            result.append([i+1, '중고 나라', name_list[i], upload_time_list[i], str(address_list[i]), int(prices), str(link_list[i]), img_link_list[i], 'low'])
         elif prices in high_np:
-            result.append([i+1, '번개 장터', name_list[i], upload_time_list[i], str(address_list[i]), int(prices), str(link_list[i]), img_link_list[i], 'high'])
+            result.append([i+1, '중고 나라', name_list[i], upload_time_list[i], str(address_list[i]), int(prices), str(link_list[i]), img_link_list[i], 'high'])
         else:
-            result.append([i+1, '번개 장터', name_list[i], upload_time_list[i], str(address_list[i]), int(prices), str(link_list[i]), img_link_list[i], 'normal'])
+            result.append([i+1, '중고 나라', name_list[i], upload_time_list[i], str(address_list[i]), int(prices), str(link_list[i]), img_link_list[i], 'normal'])
+    driver.quit()
     return result
+
